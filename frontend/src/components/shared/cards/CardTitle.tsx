b184aa8ca21287b8d09a31e3a17b03a005ee6b6d
Mariia Steeghs-Turchina <mariia.turchina@gmail.com>
2026-03-20 16:12:58 +0100

    chore: some alignment

--- frontend/src/components/shared/cards/CardTitle.tsx
+++ frontend/src/components/shared/cards/CardTitle.tsx
@@ -8,7 +8,8 @@ interface Props {
 export function CardTitle({ title, rightButtons }: Props) {
     return (
         <div className={styles.title}>
-            <span className={styles.titleText}>{title}</span>
+            {/* <span className={styles.titleText}>{title}</span> */}
+            <h1 className={styles.titleText}>{title}</h1>
             {rightButtons && <div className={styles.rightButtons}>{rightButtons}</div>}
         </div>
     );
